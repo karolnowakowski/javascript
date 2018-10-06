129a5d4e534a3485753b68532ee5782c09d64f21
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-06 12:35:11 +0200

    datatypes

---
+++ js/5-datatypes.js
@@ -0,0 +1,24 @@
+let liczba = 50;
+let tekst = "Tu będzie tekst - string";
+let trueFalse = true;
+
+let tablica = []; //tablica[0] -->1 wartosc
+let object = {
+    klucz: "wartość" //object["klucz'] lub object.klucz
+    
+};
+
+console.log(object);
+
+//ES5
+
+let number = 10;
+let zdanie = "Wyswietl " + number + " i zakoncz zadanie.";
+console.log(zdanie);
+
+
+//ES6
+
+let number2 = 10;
+let zdanie2 = `Wyswietl ${number2} i zakoncz zadanie.`;
+console.log(zdanie);
