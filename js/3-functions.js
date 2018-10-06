cccc5166abc0430bea13a4d39c7080a84af0079a
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-06 11:10:20 +0200

    3-functions

---
+++ js/3-functions.js
@@ -0,0 +1,23 @@
+//let wyswietlImie = function(imie) {
+//    
+//    return imie;
+//    
+//}
+//let wynikWyswietlImie = wyswietlImie("Krystian");
+////wyswietlImie();
+//console.log(wynikWyswietlImie);
+
+
+function dodaj (jeden, dwa, trzy) {
+    
+    let wynik = jeden + dwa + trzy;
+    return wynik;
+}
+
+let suma = dodaj (3,7,10);
+
+console.log(suma);
+
+//dodaj (3,7,10);
+
+//console.log(dodaj(3,7,10));
