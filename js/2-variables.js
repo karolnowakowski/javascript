145f82c75fdc7c18fe0258be382423319b555052
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-06 10:11:00 +0200

    pierwsze zadania

---
+++ js/2-variables.js
@@ -0,0 +1,11 @@
+console.log("Działa variables");
+let imie;
+let imieMeskie = "Kacper";
+imie = "Monika";
+imie ="Gosia";
+imie = imieMeskie;
+let liczba = 5000;
+liczba = 2000;
+const innaLiczba = 1000;
+
+console.log(liczba);
