1502fa2aed248b4f805b4017844e711a0a5d70c8
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-06 12:03:19 +0200

    functions

--- js/3-functions.js
+++ js/3-functions.js
@@ -8,16 +8,40 @@
 //console.log(wynikWyswietlImie);
 
 
-function dodaj (jeden, dwa, trzy) {
+//ES5 functions
+
+//function dodaj (jeden, dwa, trzy) {
+//    
+//    let wynik = jeden + dwa + trzy;
+//    return wynik;
+//}
+//
+//let suma = dodaj (3,7,10);
+//
+//console.log(suma);
+//
+////dodaj (3,7,10);
+//
+////console.log(dodaj(3,7,10));
+
+//ES6 functions
+
+//= jeden parametr
+//
+//let wyswietlImie = imie => imie;
+//console.log(wyswietlImie("ala"));
+
+
+
+//= wiecej niz jeden parametr
+
+//let wyswietlImie = (imie, imie2) => "imie: " + imie + "imie2: " + imie2;
     
-    let wynik = jeden + dwa + trzy;
-    return wynik;
-}
 
-let suma = dodaj (3,7,10);
+let wyswietlImie = (imie, imie2) => {
+    
+   return "imie: " + imie + " imie2: " + imie2;
+};
 
-console.log(suma);
+console.log(wyswietlImie("ala", "Karol"));
 
-//dodaj (3,7,10);
-
-//console.log(dodaj(3,7,10));
