aaea82279666e4c10d7031b507cd4ed3781d0d0e
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-06 12:03:32 +0200

    4- zasieg zmiennych

---
+++ js/4-zasieg_zmiennych.js
@@ -0,0 +1,20 @@
+let number = 5;
+
+console.log(number);
+
+function showNumber(num) {
+    let number = "w srodku " + 15;
+    console.log(number);
+    
+    if (2 +2 ==4) {
+        
+        let number = "w srodku if () " + 50;
+        console.log(number);
+    }
+    
+    return num;
+
+
+};
+
+console.log(showNumber(10));
