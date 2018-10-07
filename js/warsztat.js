18749b24934f71a1afed08a0ecf9c75b4d6a6a78
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-07 10:14:28 +0200

    warsztat

---
+++ js/warsztat.js
@@ -0,0 +1,12 @@
+function parametry(parametr1, parametr2, parametr3) {
+    
+    let wynik = parametr1 * parametr2 * parametr3;
+    
+    return wynik;
+
+}
+
+let globalna = parametry(3, 6, 2);
+
+console.log(globalna);
+
