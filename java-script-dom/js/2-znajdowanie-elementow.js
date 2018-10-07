ad8c620a7a7f04b615f388f5ae61e47843e905fe
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-07 12:52:14 +0200

    znajdowanie elementow

---
+++ java-script-dom/js/2-znajdowanie-elementow.js
@@ -0,0 +1,14 @@
+let parFirst = document.getElementById("parFirst");
+console.log(parFirst);
+
+let linki = document.getElementsByClassName("link");
+console.log(linki);
+
+let paragraph = document.getElementsByTagName("p")[1];
+console.log(paragraph);
+
+let onlyLink = document.querySelector(".link");
+console.log(onlyLink);
+
+let allLink = document.querySelectorAll(".link");
+console.log(allLink);
