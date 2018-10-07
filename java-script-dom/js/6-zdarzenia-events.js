7cfefee629cdb3cd4f6f9ee1c3f8ef0660739855
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-07 14:43:00 +0200

    events js

---
+++ java-script-dom/js/6-zdarzenia-events.js
@@ -0,0 +1,37 @@
+//add event by html dom
+
+let naszH2 = document.querySelector("#sectionFirst > h2");
+
+function pokazNaClicku () {
+    
+    console.log(naszH2.innerHTML);
+    
+}
+
+naszH2.onclick = pokazNaClicku;
+
+
+//add events by addEventListener()
+
+let mainHeader = document.querySelector("#main-header");
+
+function mouseOverEvent() {
+    
+    mainHeader.style.color = "red";
+    // this.style.color = "red"; - alternatywa
+    
+};
+
+function mouseOutEvent() {
+    
+    mainHeader.style.color = "green";
+    
+}
+
+
+mainHeader.addEventListener("mouseover", mouseOverEvent);
+mainHeader.addEventListener("mouseout", mouseOutEvent);
+
+
+mainHeader.removeEventListener("mouseover", mouseOverEvent);
+mainHeader.removeEventListener("mouseout", mouseOutEvent);
