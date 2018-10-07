3477d1a463b9cf8367c4bb2b0f18f5f101a504c0
karolnowakowski <43215868+karolnowakowski@users.noreply.github.com>
2018-10-07 10:14:42 +0200

    json przyklad

---
+++ js/json.js
@@ -0,0 +1,12 @@
+
+let jsonObject = {
+'employees': [
+{'firstName': 'John', 'lastName': 'Doe', 'zainteresowania': ["sport"]}, 
+    {'firstName': 'Anna','lastName': 'Smith'}, 
+    {'firstName': 'Peter', 'lastName': 'Jones'}
+]
+}
+
+let pierwszaOsoba = `${jsonObject.employees[0].firstName} ${jsonObject.employees[0].lastName} interesuje się ${jsonObject.employees[0].zainteresowania}`;
+
+console.log(pierwszaOsoba);
